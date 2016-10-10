bd4908e0a80e54bf9d8ffe4ffca1ecd36ef9975f
aha-oremata <sekine_y_529@msn.com>
2016-10-10 16:01:51 +0900

    Make CountlinesMojo

---
+++ src/main/java/aha/oretama/plugin/CountlinesMojo.java
@@ -0,0 +1,15 @@
+package aha.oretama.plugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * Count lines of selected files
+ * @goal countlines
+ */
+public class CountlinesMojo extends AbstractMojo {
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+    }
+}
